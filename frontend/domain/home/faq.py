dc5352299ba375422a26a9268f49b2d050f2aaed
nmagritov <nikolay.magritov@eteron.ru>
2020-06-01 20:37:30 +0300

    faq and home page content

---
+++ frontend/domain/home/faq.py
@@ -0,0 +1,4 @@
+class Faq:
+    def __init__(self, question: str, answer: str):
+        self.question = question
+        self.answer = answer
--- frontend/domain/home/home.py
+++ frontend/domain/home/home.py
@@ -1,12 +1,17 @@
 from frontend.domain.home.carousel_item import CarouselItem
+from frontend.domain.home.faq import Faq
 
 
 class Home:
-    header = "Заголовок"
-    description = "Описалово"
+    header = "Добро пожаловать в мой уютный уголок!"
+    description = "Здесь в захватывающей карусели вы сможете найти все на что этот проект способен. Тогда чего " \
+                  "вы ждете? Вперед!"
     carousel = [
         CarouselItem(image='carousel/home.png'),
         CarouselItem(image='carousel/about.png', ref="about"),
     ]
-    info_header = "Информация"
-    infos = []
+    faq_header = "Информация"
+    faq_description = 'Здесь Вы найдете ответы на самые часто задаваемые вопросы:'
+    faqs = [
+        Faq(question='Что здесь происходит?', answer='Да кто его вообще знает!'),
+    ]
